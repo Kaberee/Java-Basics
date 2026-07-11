da5475c459270890cd4439a57bef1b11643c18d4
Kaberee <tasnimasheikh74@gmail.com>
2026-07-11 12:07:52 +0600

    Implement even number series and sum calculation
    Prints even numbers from 2 to 20 and calculates their sum.

---
+++ Series_sum(even).java
@@ -0,0 +1,25 @@
+public class Series{
+    public static void main(String[] args) {
+     for(int i=2; i<=20; i+=2)
+     {
+         System.out.println(i+" ");
+     }
+     int j=2;
+     while(j<=20)
+     {
+     System.out.println(j+ " ");
+    j+=2;
+     }
+     int k=2;
+     do {
+        System.out.println(k);
+        k+=2;
+     }while(k<=20);
+     int sum=0;
+     for(int i=2; i<=20; i+=2)
+     {
+         sum=sum+i;
+     }
+     System.out.println(sum);
+    }
+}
